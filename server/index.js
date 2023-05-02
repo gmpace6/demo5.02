76a4966574c65f25d3151b0f63a43b33177e777c
Garrett Pace <elderpace6@gmail.com>
2023-05-02 13:24:14 -0600

    update

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-const {getMovies} = require('./controller.js')
+const { getMovies, addMovie, updateMovie } = require("./controller.js");
 
-app.get('/api/movies', getMovies)
+app.get("/api/movies", getMovies);
+app.post("/api/movies", addMovie);
+app.put("./api/movies/:id", updateMovie);
 
-app.listen(4004, () => console.log('Darth Mickey on port 4004'))
-
+app.listen(4004, () => console.log("Darth Mickey on port 4004"));
